5fa4788cf140a4cda6c71407c951c4c57baf0b29
Happy Cat Toys <dev@happycattoys.ca>
2026-02-08 09:49:22 -0500

    feat: add Tawk.to live chat widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,21 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6988a0c34f57c41c38997abe/1jgur7vnh';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   )
